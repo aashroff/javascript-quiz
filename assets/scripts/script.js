e0f6def50237854fe82cca87346969eb3432472e
Ashin Ashroff <ashin.ashroff@yahoo.com>
2023-01-23 01:09:35 +0000

    Added building blocks for how questions are displayed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,25 @@
-let startButton = document.querySelector("#start");
-let timer = document.querySelector("#time");
-let startScreen = document.querySelector("#start-screen");
+const startButton = document.querySelector("#start");
+const timer = document.querySelector("#time");
+const startScreen = document.querySelector("#start-screen");
+const questions = document.querySelector("#questions");
+const choices = document.querySelector("#choices");
+const questionTitle = document.querySelector("#question-title")
 let startingTime = 75;
+
+// Questions Object
+const questionsObj = {
+
+    question : [
+        "Inside which HTML element do we put the JavaScript?"
+    ],
+    choices : [
+        ["<js>", "<scripting>", "<script>", "<javascript>"],
+
+
+    ]
+
+}
+
 
 // Start Timer
 
@@ -13,11 +31,31 @@ function startTimer(){
 }
 
 
+function displayQuestion(){
+
+    let questionCount = 0;
+
+    questions.classList.remove("hide")
+    questionTitle.innerText = questionsObj.question[0];
+   
+    for(elem of questionsObj.choices[0]){
+        const answerChoice = document.createElement("button");
+        answerChoice.innerText = elem;
+        choices.appendChild(answerChoice);
+    }
+    
+
+};
+
+
 // Execute this when the quiz is started
 function startQuiz(){
-    
+    // Set display of start screen div to none
+    startScreen.style.display = "none";
     timer.textContent = startingTime;
+    displayQuestion();
     setInterval(startTimer, 1000);
+
 }
 
 
