300bd69064073d02686a49f36df5cfe031de548a
Ashin Ashroff <ashin.ashroff@yahoo.com>
2023-01-23 14:52:52 +0000

    Implemented questions and answer checking logic

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,8 +4,8 @@ const startScreen = document.querySelector("#start-screen");
 const questions = document.querySelector("#questions");
 const choices = document.querySelector("#choices");
 const questionTitle = document.querySelector("#question-title")
-let correctAnswerSfx = new Audio('./assets/sfx/correct.wav');
-let inCorrectAnswerSfx = new Audio('./assets/sfx/incorrect.wav');
+const correctAnswerSfx = new Audio('./assets/sfx/correct.wav');
+const inCorrectAnswerSfx = new Audio('./assets/sfx/incorrect.wav');
 let startingTime = 75;
 
 // Questions Object
@@ -15,8 +15,25 @@ const questionsObj = [
         question : "Inside which HTML element do we put the JavaScript?",
         choices : ["<js>", "<scripting>", "<script>", "<javascript>"],
         answer: "<script>"
+    },
+    {
+        question : "Common used data types DO NOT include:",
+        choices : ["strings", "booleans", "alert", "numbers"],
+        answer: "alert"
+    },
 
+    {
+        question : "Arrays in JavaScript can be used to store ______",
+        choices : ["Other Arrays", "Objects", "Strings", "All of the above."],
+        answer: "All of the above."
+    },
+
+    {
+        question : "Which variable declaration allows \"Hoisting\" in JavaScript?",
+        choices : ["var", "let", "const", "None of the above."],
+        answer: "var"
     }
+
 
 ]
 
@@ -32,34 +49,67 @@ function startTimer(){
 
 
 function renderQuestion(index) {
+    if(index <= 3){
+        questionTitle.innerText = questionsObj[index].question;
+    
+        //reset the choices on each event
+        choices.innerHTML = "";
+        for(elem of questionsObj[index].choices){
+            const answerChoice = document.createElement("button");
+            answerChoice.innerText = elem;
+            choices.appendChild(answerChoice);
+            answerChoice.addEventListener("click", function() {
+                if(answerChoice.innerText != questionsObj[index].answer) {
+                    //When answer is incorrect reduce time by 10 and play the incorrect answer sfx
+                    if(timer.textContent >= 10){
+                        timer.textContent -= 10;
+                    }
+                    else{
+                        timer.textContent = 0;
+                    }
+                    inCorrectAnswerSfx.play();
+                    if(index < 3){
+                        index+=1;
+                        renderQuestion(index);
+                    }
+                    // If there are no more questions
+                    else{
 
-    questionTitle.innerText = questionsObj[index].question;
-    for(elem of questionsObj[index].choices){
-        const answerChoice = document.createElement("button");
-        answerChoice.innerText = elem;
-        choices.appendChild(answerChoice);
-        answerChoice.addEventListener("click", function() {
-            if(answerChoice.innerText != questionsObj[index].answer) {
-                console.log("Incorrect Answer!");
-                inCorrectAnswerSfx.play();
-            }
-            else{
-                console.log("Correct Answer!");
-                correctAnswerSfx.play();
-            }
-        })
+                        console.log("No More questions")
+
+                    }
+                    
+                }
+                else{
+                    //When the answer is correct, play the correct answer sfx, and ask the next question.
+                    correctAnswerSfx.play();
+                    if(index < 3){
+                        index+=1;
+                        renderQuestion(index);
+                    }
+                    else{
+
+                        console.log("No More questions")
+
+                    }
+                    
+                    
+                    
+                }
+            })
+        }
     }
 }
 
 
 function displayQuestion(){
 
-    //let questionCount = 0;
 
     questions.classList.remove("hide")
     
     // Ask first question
     renderQuestion(0);
+  
     
 
 };
